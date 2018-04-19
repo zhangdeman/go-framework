91ac7b6fdeb024d545fea11e97d8fb64ba7e3ca4
zhangdeman <zhangdeman@didichuxing.com>
2018-04-19 11:41:23 +0800

    增加引导模块

---
+++ core/boot/boot.go
@@ -0,0 +1,43 @@
+package boot
+
+import (
+	"server"
+)
+
+var (
+	RunConfigInstance RunConfig
+)
+/**
+ * 初始化函数
+ */
+func init()  {
+	RunConfigInstance = RunConfig{}
+	//初始化服务器
+	newServer := server.MakeServer("http", []string{}, []string{}, ":8990")
+	//初始化配置
+	//newServer.AddUriMap()
+	RunConfigInstance.RunServer = newServer
+	//运行服务器
+	RunConfigInstance.RunServer.RunServer()
+}
+
+/**
+ * 运行引导接口
+ */
+type RunBootInterface interface {
+
+}
+
+/**
+ * 运行引导结构体
+ */
+type RunBoot struct {
+
+}
+
+/**
+ * 运行之后的一些配置
+ */
+type RunConfig struct {
+	RunServer server.NewServerInterface
+}
